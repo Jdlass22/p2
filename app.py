2db0924ef3a25a5c0ed8b3408f68a97532335ff7
Jdlass22 <147341350+Jdlass22@users.noreply.github.com>
2023-10-08 20:31:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from gtts import gTTS
 from PIL import Image
 
 
-st.markdown("<h1 style='text-align: center; color: red;'>Asistente Sonoro</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: orange;'>Asistente Sonoro</h1>", unsafe_allow_html=True)
 image = Image.open('WORLD-FLAGS.png')
 
 st.image(image, width=200)
